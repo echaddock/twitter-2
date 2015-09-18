8054df3fd385cd49b01d68fa1fca18c60becf7f6
Liz Chaddock <lizchaddock@unknown-10-73-205-x.yahoo.com>
2015-09-18 15:49:47 -0700

    Initial Commit

---
+++ Twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter
+//
+//  Created by Liz Chaddock on 9/18/15.
+//  Copyright © 2015 Liz Chaddock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitter
+//
+//  Created by Liz Chaddock on 9/18/15.
+//  Copyright © 2015 Liz Chaddock. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
